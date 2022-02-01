e155335d4da9176d46cb98e5999af6179a0b0bf2
Dasha Daniyalian <dasha.daniyalian@codeandtheory.com>
2022-02-01 13:45:12 -0600

    search functionality test

---
+++ Test Suites/search.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>search</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>04de1b48-bf97-4ff4-94cd-3b4cd08cad61</testSuiteGuid>
+   <testCaseLink>
+      <guid>cd50c60d-7d81-4bba-8f1f-569588225c4d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
